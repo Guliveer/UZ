e14edf5c59d629586433d326eb63cc7df7adac8f
Oliwer Pawelski <oliwer.pawelski@gmail.com>
2024-04-16 22:50:38 +0200

    Renamed few folders and added task from new tasks list
    Lista_1 -> Lista_01
    Lista_2 -> Lista_02
    + Lista_03/zad01

---
+++ R1S2/Programowanie obiektowe (L)/Lista_03/zad01/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+>
+CMakeLists.txt,9/a/9a2aa4db38d3115ed60da621e012c0efc0172aae
+8
+main.cpp,6/8/682a50d19e416195436708e48c7761923b8d6651
---
+++ R1S2/Programowanie obiektowe (L)/Lista_03/zad01/main.cpp
@@ -0,0 +1,65 @@
+/*
+ * Rysunek pomocniczy:
+ *
+ *  p |  o--|---->|  1  /  o--|---->|  5  /  o  |
+ *       ⬆️                                 ⬇️
+ *       ---------------------------|--o  /  7  |
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+struct Elem3;
+struct Elem1 {
+    int val;
+    Elem3 *ptr;
+};
+
+struct Elem2 {
+    int val;
+    Elem1 **ptr;
+};
+
+struct Elem3 {
+    int val;
+    Elem2 *ptr;
+};
+
+int
+main() {
+    Elem1 *p; // wskaźnik na strukturę Elem1
+
+    p = new Elem1; // alokacja pamięci dla struktury Elem1
+    (*p).val = 1; // przypisanie wartości do pola val struktury Elem1
+    (*p).ptr = new Elem3;
+    (*((*p).ptr)).val = 5;
+    (*((*p).ptr)).ptr = new Elem2;
+    (*((*((*p).ptr)).ptr)).val = 7;
+    (*((*((*p).ptr)).ptr)).ptr = &p; // wskaźnik na wskaźnik; powrót do p
+
+    cout
+            << "p->val = "
+            << p->val
+            << endl;
+    cout
+            << "p->ptr->val = "
+            << p->ptr->val
+            << endl;
+    cout
+            << "p->ptr->ptr->val = "
+            << p->ptr->ptr->val
+            << endl;
+    cout
+            << "p->ptr->ptr->ptr->val = "
+            << (*p->ptr->ptr->ptr)->val
+            << endl;
+
+    delete (*((*p).ptr)).ptr; // zwolnienie pamięci
+    delete (*p).ptr;
+    delete p;
+    p = nullptr; // wartość 'nullptr', bo inaczej p wskazuje śmietnik
+
+    return EXIT_SUCCESS;
+}
