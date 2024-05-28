8efca51bc8e7e70e1f953074b4fa9a84e8b74aae
Oliwer Pawelski <oliwer.pawelski@gmail.com>
2024-05-28 14:03:49 +0200

    Fixed Lab_03/zad1.ml

--- R1S2/Podstawy systemów dyskretnych (L)/Lab 03/zad1.ml
+++ R1S2/Podstawy systemów dyskretnych (L)/Lab 03/zad1.ml
@@ -3,18 +3,18 @@ wyznaczający Największy Wspólny Dzielnik (NWD). *)
 
 
 (* 'If' method *)
-let rec gcd a b =
+let rec gcd_if a b =
   if b = 0 then
     a
   else
-    gcd b (a mod b);;
+    gcd_if b (a mod b);;
 
 
 (* 'Match' method *)
 let rec gcd_match a b =
   match b with
   | 0 -> a
-  | _ -> gcd b (a mod b);;
+  | _ -> gcd_match b (a mod b);;
 
 let result a b =
   print_endline ("NWD(" ^ string_of_int a ^ ", " ^ string_of_int b ^ ") = " ^ string_of_int (gcd a b));;
