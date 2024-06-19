4a50862bd4c3f15edd2486e09c5f79441e5b1f1d
Oliwer Pawelski <oliwer.pawelski@gmail.com>
2024-06-19 23:18:56 +0200

    Added side note

--- R1S2/Programowanie obiektowe (W)/zad02/main.cpp
+++ R1S2/Programowanie obiektowe (W)/zad02/main.cpp
@@ -12,7 +12,7 @@ public:
     // Zdefiniuj operator(), który przyjmuje 2 argumenty int "idx" i double "wartosc"
     // i ma za zadanie wstawić do kontenera "wartosc" w miejsce o nnumerze "idx".
     // Funkcja zwraca 1 jeśli operacja się powiodła lub 0, gdy kontener nie posiada miejsca o danym indeksie "idx".
-    int operator()(int idx, double wartosc) {
+    int operator()(int idx, double wartosc) { // ewentualnie typu "bool" zamiast "int"
         if (idx < 0 || idx >= roz) {
             return 0;
         }
@@ -29,6 +29,6 @@ int main() {
     Kontener j(5);
     std::cout << j(3, 3.14) << std::endl; // result: 1
     std::cout << j(5, 3.14) << std::endl; // result: 0
-    
+
     return EXIT_SUCCESS;
 }
